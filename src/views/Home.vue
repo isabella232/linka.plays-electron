b2ca316ed5393b7a9aa14f4f1c7b894eb3028612
Ivan Bakaidov <ivan@aacidov.ru>
2021-08-09 13:25:38 +0300

    fix main menu grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <h1>Игры</h1>
     <v-container>
       <v-row>
-        <v-col cols="4">
-          <v-card v-for="game of games" :key="game.id">
+        <v-col cols="4" v-for="game of games" :key="game.id" >
+          <v-card >
             <v-card-title primary-title> {{ game.title }} </v-card-title>
             <v-card-text>
               <p class="">{{ game.description }}</p>
