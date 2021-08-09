310ff5259643a57609b838ffc03fa3c479720692
Ivan Bakaidov <ivan@aacidov.ru>
2021-08-09 13:47:16 +0300

    fix sound player

--- src/Games/Game.ts
+++ src/Games/Game.ts
@@ -2,12 +2,29 @@ import Vue from "vue";
 
 export class Game extends Vue {
     sounds = {
-        good: new Audio('/sounds/good.wav')
+        good: new Audio('/sounds/good.wav'),
+        newTask: new Audio('/sounds/newTask.wav'),
     }
     points = 0;
+    errors = 0;
+    step = 0;
+    maxSteps = 10;
+
 
     addPoint() {
         this.points++;
         this.sounds.good.play()
+        this.$emit('pointChanged', this.points)
+    }
+    nextStep(){
+        this.step++;
+        this.sounds.newTask.pause()
+        this.sounds.newTask.currentTime = 0;
+        this.sounds.newTask.play();
+        this.$emit('stepChanged', this.step)
+
+        if(this.step===this.maxSteps){
+            this.$emit('gameEnd')
+        }
     }
 }
